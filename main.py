9875128f2b505d8b6d0417b8596a73e2fdcdc7cb
mikaeloganesian <mikaeloganesian2005@mail.ru>
2023-02-19 12:20:50 +0300

    19.02.2023_2

--- main.py
+++ main.py
@@ -13,14 +13,34 @@ root.title('Delta - шпаргалка по геометрии')
 root.state('zoomed')
 root.protocol('WM_DELETE_WINDOW', finish)
 
-# UI inicializating
+# UI creating
+helloLabel = Label(root, text='Hello User, this is Delta', font=('Arial', '20', 'bold'), width=20, pady=8)
+
+planemetryFrame = Frame(root, borderwidth=3, relief=SOLID)
+planemetryLabel = Label(planemetryFrame, text='Планеметрия', font=('Arial', '14', 'bold'), width=50, height=15, anchor='nw', padx=20, pady=10)
+
+stereometryFrame = Frame(root, borderwidth=3, relief=SOLID)
+stereometryLabel = Label(stereometryFrame, text='Стереометрия', font=('Arial', '14', 'bold'), width=50, height=15, anchor='nw', padx=20, pady=10)
+
+# UI pack
+helloLabel.pack()
+
+planemetryFrame.pack(anchor='w', pady=20, padx=20)
+planemetryLabel.pack()
+
+stereometryFrame.pack(anchor='w', pady=20, padx=20)
+stereometryLabel.pack()
+
+#buttons creating + packing
 
 i = 0
 for r in range(3):
     for c in range(3):
-        category = ttk.Button(text=categoryName[i], width='50', padding='10')
-        category.grid(row=r, column=c)
+        button = Button(planemetryFrame, text=categoryName[i], width=12)
+        button.pack()
         i+=1
+
+
 
 # open the window
 root.mainloop()
